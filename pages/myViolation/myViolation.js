a61c998273b56992dcdae0893342bf121f145750
fancycoderzf <fancymezf@qq.com>
2020-08-29 04:37:15 +0800

    修改了我的预约功能
    有待进一步完善（并且发现一个新问题：openid其实未获取到，需另想办法)

--- pages/myViolation/myViolation.js
+++ pages/myViolation/myViolation.js
@@ -7,90 +7,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activeNames: ['1'],
-    Violated: false
+    Violated: false,
+    manageIDforUser: "",
+    stallID: "",
+    item: "",
+    recordTime: "",
+    remarks: "",
+    index: ""
 
   },
-
-  showViolation: function (res) {
-    //console.log(res.detail.length)
-    if (res.detail.length == 2) {
-      //console.log("展开面板")
-      this.search()
-      setTimeout(() => {
-        wx.hideLoading();
-        if (this.data.Violated) {
-          //console.log("拿到数据了呀")
-          this.setData({
-            activeNames: res.detail,
-          });
-        } else {
-          Notify({
-            type: 'warning',
-            message: '当前无违规记录'
-          })
-        }
-      }, 1000)
-    } else {
-      //此处主要是如果搜到数据而且展开面板之后可以调用这里折叠面板
-      this.setData({
-        activeNames: res.detail,
-      });
-    }
-  },
-
-  search() {
-    var that = this
-    wx.showLoading({ //弹出框显示内容，当出现hideloading时消失
-      title: '加载中',
+  setNavigationBar: function () {
+    wx.setNavigationBarTitle({
+      title: '我的违规',
     })
-    db.collection('violation')
+  },
+
+  getManageID: function(){
+    var that = this
+    var openid;
+    wx.getStorage({
+      key: 'openid',
+      success: function (res) {
+        openid = res.data;
+        console.log('get-id');
+      }
+    })
+
+    db.collection('apply')
       .where({
-        _openid: wx.getStorageSync('openid')
+        _openid: openid,
+       condition: "1"
       })
       .get({
-        success(res) {
-          //console.log(res.data.length)
-          if (res.data.length == 0) {
-            //console.log("执行了")
-            that.setData({
-              Violated: false
-            })
-          } else {
-            that.setData({
-              Violated: true,
-              brand: res.data[0].brand,
-              Item: res.data[0].Item,
-              id: res.data[0]._id,
-              recordTime: res.data[0].recordTime.toLocaleString()
-            })
-          }
-
-        },
-        fail() {
-          console.log("当前无违规记录")
+        success: function(res) {
+          console.log('get-manageID');
+          console.log('res' + res.data[1].manageID);
+          that.setData({
+            manageIDforUser: res.data[1].manageID
+            // brand: res.data[0].brand,
+            // Item: res.data[0].Item,
+            // id: res.data[0]._id,
+            // recordTime: res.data[0].recordTime.toLocaleString()
+          })
+    },
+        fail: function(res) {
+          console.log("当前无有资质品牌信息");
         }
       })
+
+  },
+  showViolation: function () {
+      var that = this
+      console.log(that.data)
+      console.log(that.data.manageIDforUser)
+      db.collection('Violation')
+      .where({
+        manageID: that.data.manageIDforUser
+      }).get({
+        success: function(res) {
+          that.setData({
+            stallID: res.data[0].stallID,
+            item: res.data[0].item,
+            recordTime: res.data[0].recordTime.toLocaleString(),
+            remarks: res.data[0].remarks,
+            Violated: true
+          })
+        },
+        fail: function(res) {
+          console.log("查询违规记录失败");
+        }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that=this
+    wx.showLoading({ //弹出框显示内容，当出现hideloading时消失
+      title: '加载中',
+    })
+    console.log('start - setnav');
+    that.setNavigationBar();
+    console.log('start - get');
+    that.getManageID();
+    console.log('start - show');
+    setTimeout(() => {
+      that.showViolation();
+      } 
+    , 1500)
+    setTimeout(() => {
+      wx.hideLoading();
+      if (that.data.Violated==false) {
+        Notify({
+          type: 'warning',
+          message: '当前无违规信息'
+        })
 
+      } 
+    }, 3000)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // that.showViolation();
   },
 
   /**
