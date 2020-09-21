03d9f94b6ddd5c3cf955fd924ac82c388aa02a36
fancycoderzf <fancymezf@qq.com>
2020-09-21 12:07:05 +0800

    冲突解决

--- pages/startReserve/startReserve.js
+++ pages/startReserve/startReserve.js
@@ -73,25 +73,6 @@ Page({
       areaWidth
     })
   },
-  //以上为有关滑块的函数
-
-  getMoveableArea: function () {
-    //可移动区域的宽度
-    // todo
-    // 这里的宽度:屏幕宽度; 这里的高度:屏幕高度 - 状态栏高度;
-    var windowWidth = App.globalData.screenWidth
-    var windowHeight = App.globalData.screenHeight
-    // console.log(windowWidth)
-    // console.log(windowHeight)
-    var areaHeight = windowHeight -77
-
-    // 这里的函数一定要随项目进度更改，目前这样设置是为了减去上面三个单元格的高度
-    var areaWidth = windowWidth
-    this.setData({
-      areaHeight,
-      areaWidth
-    })
-  },
 //有关滑块的函数
 lowValueChangeAction: function (e) {//改变左滑块
   var leftHour=Math.floor(e.detail.lowValue / 60);//滑块左边的小时
