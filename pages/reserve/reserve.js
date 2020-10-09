aebb73f24c55b497956af66862fdac999c9b05c3
被制作时长两年半的个人练习生 <oncwnuGD-rwPoA0lcWtWQeFAxrBo@git.weixin.qq.com>
2020-10-09 10:47:02 +0800

    地理信息权限修复

--- pages/reserve/reserve.js
+++ pages/reserve/reserve.js
@@ -74,24 +74,26 @@ Page({
   //     scale: scale
   //   });
   // },
-  postAddress: function () {
-    var that = this
-    wx.getSetting({
-      success(res) {
-        if (!res.authSetting['scope.userLocation']) {
-          wx.openSetting({
-            success: function (res) {
-              if (res.authSetting['scope.userLocation']) {
-                this.getAddress()
-              }
-            }
-          })
-        } else {
-          this.getAddress()
-        }
-      }
-    })
-  },
+  // postAddress: function () {
+  //   var that = this
+  //   wx.getSetting({
+  //     success(res) {
+  //       if (!res.authSetting['scope.userLocation']) {
+  //         wx.openSetting({
+  //           success: function (res) {
+  //             if (res.authSetting['scope.userLocation']) {
+  //               this.getAddress()
+  //             } else {
+
+  //             }
+  //           }
+  //         })
+  //       } else {
+  //         this.getAddress()
+  //       }
+  //     }
+  //   })
+  // },
   //如果成功的话就获得用户地理位置
   getAddress: function () {
     var that = this
@@ -190,12 +192,131 @@ Page({
   onShow: function () {
     this.getAddress()
     this.onFocus()
+    this.getUserLocation()
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         active: 1
       });
     }
   },
+
+  getUserLocation: function () {
+
+    var _this = this;
+
+    wx.getSetting({
+
+      success: (res) => {
+
+        // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+
+        // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+
+        // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+
+          //未授权
+
+          wx.showModal({
+
+            title: '请求授权当前位置',
+
+            content: '需要获取您的地理位置，请确认授权',
+
+            success: function (res) {
+
+              if (res.cancel) {
+
+                //取消授权
+
+                wx.showToast({
+
+                  title: '拒绝授权',
+
+                  icon: 'none',
+
+                  duration: 1000
+
+                })
+                wx.switchTab({
+                  url: '/pages/apply/apply',
+                })
+
+              } else if (res.confirm) {
+
+                //确定授权，通过wx.openSetting发起授权请求
+
+                wx.openSetting({
+
+                  success: function (res) {
+
+                    if (res.authSetting["scope.userLocation"] == true) {
+
+                      wx.showToast({
+
+                        title: '授权成功',
+
+                        icon: 'success',
+
+                        duration: 1000
+
+                      })
+
+                      //再次授权，调用wx.getLocation的API
+
+                      _this.geo();
+
+                    } else {
+
+                      wx.showToast({
+
+                        title: '授权失败',
+
+                        icon: 'none',
+
+                        duration: 1000
+
+                      })
+
+                    }
+
+                  }
+
+                })
+
+              }
+
+            }
+
+          })
+
+        } else if (res.authSetting['scope.userLocation'] == undefined) {
+
+          //用户首次进入页面,调用wx.getLocation的API
+
+          _this.getAddress()
+
+        } else {
+
+          console.log('授权成功')
+
+          //调用wx.getLocation的API
+
+          _this.getAddress()
+
+        }
+
+      }
+
+    })
+
+
+
+  },
+
+
+
 
   /**
    * 生命周期函数--监听页面隐藏
